9c58a7bd380dc0810166c0190f12b9f580b5a15b
aupaprunia <aupaprunia@gmail.com>
2021-03-28 18:13:38 +0530

    new file:   HooHacks/2.png 	renamed:    HooHacks/chart.html -> HooHacks/acc_chart.html 	new file:   HooHacks/activity_chart.html 	new file:   HooHacks/calm.png 	new file:   HooHacks/flex_chart.html 	modified:   HooHacks/index.html 	new file:   HooHacks/login.html 	new file:   HooHacks/login_style.css 	modified:   HooHacks/main.js 	new file:   HooHacks/photo.jpg 	modified:   HooHacks/style.css 	deleted:    HooHacks/test.html 	deleted:    HooHacks/test.js

--- HooHacks/main.js
+++ HooHacks/main.js
@@ -1,104 +1,53 @@
-// CODE DUMP FOR HTML FILES!!!
-
-var api_url = "https://trialproject-55deb-default-rtdb.firebaseio.com/Devices/DeviceID1.json";
-var counter_url ="https://trialproject-55deb-default-rtdb.firebaseio.com/CTR.json";
+var api_url = "https://smartwidget-ae5c7-default-rtdb.firebaseio.com/Website/DeviceId1";
+var counter_url ="https://smartwidget-ae5c7-default-rtdb.firebaseio.com/Website/ctr.json";
+var link;
 
 function getCounter(){    
     fetch(counter_url).then(response=>{
     return response.json();
     }).then(counter=>{
-        
-
+        console.log(counter);
 for(var i = 1; i <= counter; i++){
-    console.log("hello");
-}
-// function fetchData(){    
-// fetch(api_url).then(response=>{
-// return response.json();
-// }).then(answer=>{
-//     console.log(answer);
-//})
-//})
+    var str = i.toString();
+    var slash = "/"
+    link = api_url.concat(slash.concat(str.concat(".json")));
 
-//fetchData();
+    async function getData(){
+        const response = await fetch(link);
+        const data =await response.json();
 
+        const {acc, activity, flex, re, timestamp} = data;
+        console.log(acc,activity,flex,re,timestamp);
+        }
+        getData();
+    }//for loop
+    return link;
+}).then(final_url=>{
+    async function getData(){
+            const response = await fetch(final_url);
+            const data =await response.json();
 
+            document.getElementById("Accelerometer").textContent= Math.round(data.acc * 100) / 100;
+            document.getElementById("Flex").textContent= Math.round(data.flex * 100) / 100;
+            document.getElementById("RE").textContent= Math.round(data.re * 100) / 100;
 
+            if (data.activity == 1){
+                document.getElementById("Activity").textContent= "Low";
+            } 
 
+            if (data.activity == 2){
+                document.getElementById("Activity").textContent= "Medium";
+            } 
 
+            if (data.activity == 3){
+                document.getElementById("Activity").textContent= "High";
+            } 
+            document.getElementById("latest_time").textContent= data.timestamp;
 
+    }
+    getData();
+}) 
+}//getCounter
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const api_url = "https://trialproject-55deb-default-rtdb.firebaseio.com/product.json";
-// async function getData(){
-//     const response = await fetch(api_url);
-//     const data =await response.json();
-//     //#1
-//     document.getElementById("name").textContent= data.name;
-//     document.getElementById("price").textContent= data.price;
-//     document.getElementById("description").textContent= data.description;
-//     //#2
-//     const {name, description, price} = data; // const{name, desc, price}={x:maggie, y:12, z: noodle}
-
-//     console.log(name, description,price);
-//     makeChart(name, description, price);
-// }
-
-// getData();
-
-// function makeChart(n, d, p){
-//     //console.log(n,d,p);
-
-//     var ctx = document.getElementById('chart').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [name, desc, pri, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
-
-// }
+getCounter();
+setInterval(getCounter, 2000);
